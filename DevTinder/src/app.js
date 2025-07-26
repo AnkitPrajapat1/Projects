1d2dfba5270c460a3eefdc41b57545c330040bd3
AnkitPrajapat1 <ankitprajapat403@gmail.com>
2025-07-26 18:06:20 +0530

    added schema method for password validation and jwt token generation

--- DevTinder/src/app.js
+++ DevTinder/src/app.js
@@ -4,25 +4,24 @@ const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const validator = require("validator");
 const app = express();
-const cookieParser=require("cookie-parser")
-const jwt=require("jsonwebtoken")
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 const User = require("../src/model/user.js");
 const connectDB = require("./config/database.js");
-const {userAuth}=require("../middleware/auth.js")
+const { userAuth } = require("../middleware/auth.js");
 
-
-app.use(express.json()); 
+app.use(express.json());
 app.use(cookieParser());
 app.post("/signup", async (req, res) => {
   try {
     //validaation of data
     validateSignUpData(req);
-    
+
     const { firstName, lastName, emailId, password } = req.body;
     //Encrypting password
     const passwordHash = await bcrypt.hash(password, 10);
     // const userObj = req.body;
-    
+
     //creating a new instance of the User model
     const user = new User({
       firstName,
@@ -46,44 +45,49 @@ app.post("/login", async (req, res) => {
     }
 
     const user = await User.findOne({ emailId: emailId });
-    
+
     if (!user) {
       throw new Error("Invalid Credentials");
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    
-    if (!isPasswordValid) {
+    const isPasswordValid = await user.validatePassword(password)
+    if (isPasswordValid) {
+      //create a jwt token
+      const token = await user.getJWT();
+      //add the token to cookie and send the response back to the user
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 24 * 3600000), // cookie will be removed after 8 hours
+      });
+      res.send("Login successfull");
+    } else { 
       throw new Error("Invalid Credentials");
     }
-    //create a jwt token
-    const token= jwt.sign({_id:user._id},"Ankit?710");
-    //add the token to cookie and send the response back to the user
-    res.cookie("token", token);
-    res.send("Login successfull");
-  } catch (err) { 
+  } catch (err) {
     res.status(400).send("Error: " + err.message);
   }
 });
 
-app.get("/profile",userAuth,async(req,res)=>{
-   try{
-    const user=req.user;
+app.get("/profile", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
     // console.log(cookies)
     res.send(user);
-  }catch (err) { 
+  } catch (err) {
     res.status(400).send("Error: " + err.message);
   }
-})
+});
 
-
+app.post("/sendConnectionRequest", userAuth, async (req, res) => {
+  const user = req.user;
+  console.log("Sending connection reequest");
+  res.send(user.firstName + " send connection request!");
+});
 
 connectDB()
-.then(() => {
-  console.log("Database connected");
-  app.listen(3000, () => {
-    console.log("Server running on port 3000");
-  });
-})
-.catch((err) => console.log("Database not connected", err));
-
+  .then(() => {
+    console.log("Database connected");
+    app.listen(3000, () => {
+      console.log("Server running on port 3000");
+    });
+  })
+  .catch((err) => console.log("Database not connected", err));
--- DevTinder/src/model/user.js
+++ DevTinder/src/model/user.js
@@ -1,74 +1,89 @@
 const mongoose = require("mongoose");
-const validator=require("validator")
-const userSchema=new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true,
-        minLength:3,
-        maxLength:50,
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
     },
-    lastName:{
-        type:String
+    lastName: {
+      type: String,
     },
-    emailId:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-        trim:true,
-        // validate(value){
-        //     if(!validator.isEmail(value)){
-        //         throw new Error("Email is invalid")
-        //     }
-         // }
-         validate: {
-      validator: (v) => validator.isEmail(v),
-      message: 'Invalid email format'
-    }
-        
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      // validate(value){
+      //     if(!validator.isEmail(value)){
+      //         throw new Error("Email is invalid")
+      //     }
+      // }
+      validate: {
+        validator: (v) => validator.isEmail(v),
+        message: "Invalid email format",
+      },
     },
-    
-    password:{
-        type:String,
-        required:true,
-        validate(value){
-            if(!validator.isStrongPassword(value)){
-                throw new Error("Password is not strong enough")
-            }
+
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Password is not strong enough");
         }
+      },
     },
-    age:{
-        type:Number,
-        min:18,
-        max:100,
+    age: {
+      type: Number,
+      min: 18,
+      max: 100,
     },
-    gender:{
-        type:String,
-        validate(value){
-            if(!["male","female","others"].includes(value)){
-                throw new Error("Gender should be either male female or others")
-            }
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender should be either male female or others");
         }
+      },
     },
-    photoUrl:{
-        type:String,
-        default:"https://geographyandyou.com/images/user-profile.png",
-        validate(value){
-            if(!validator.isURL(value)){
-                throw new Error("Photo url is invalid")
-            }
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Photo url is invalid");
         }
+      },
     },
-    about:{
-        type:String,
-        default:"default value for about"
+    about: {
+      type: String,
+      default: "default value for about",
     },
-    skills:{
-        type:[String],
-    }
+    skills: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
 
-},{timestamps:true});
+userSchema.methods.getJWT = async function () {
+    const user=this;
+ const token= await jwt.sign({ _id: user._id }, "Ankit?710", { expiresIn: "1d" });
 
+ return token 
+};
 
-module.exports =mongoose.model("User",userSchema);
+userSchema.methods.validatePassword=async function(passwordInputByUser){
+  const user=this;
+  const hashedPassword=user.password;  
+  const isPasswordValid=await bcrypt.compare(passwordInputByUser,hashedPassword);
+  return isPasswordValid
+}
+module.exports = mongoose.model("User", userSchema);
